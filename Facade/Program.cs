3127cf9f45260cd58004e4f5a4ca322ee3383e0e
wanasak <u510610433@gmail.com>
2017-10-11 16:58:51 +0700

    facade

---
+++ Facade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Facade
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server();
+
+            Console.WriteLine("Hello! I'll be your server today. What is your name?");
+            var name = Console.ReadLine();
+
+            var patron = new Patron(name);
+
+            Console.WriteLine($"Hello {patron.Name}. What appetizer would you like? (1 - 15):");
+            var appId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"What entree would you like? (1 - 10):");
+            var entreeId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"What drink would you like? (1 - 5):");
+            var drinkId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("I'll get that order in right away.");
+
+            // Here where the facade simplifies
+            server.PlaceOrder(patron, appId, entreeId, drinkId);
+        }
+    }
+}
---
+++ Facade/Restuarant.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Facade
+{
+    class Patron
+    {
+        private string _name;
+
+        public Patron(string name)
+        {
+            _name = name;
+        }
+
+        public string Name { get => _name; }
+    }
+
+    class FoodItem { public int DishId; }
+
+    interface KitchenSection
+    {
+        FoodItem PrepDish(int dishId);
+    }
+
+    class Order
+    {
+        public FoodItem Appetizer { get; set; }
+        public FoodItem Entree { get; set; }
+        public FoodItem Drink { get; set; }
+    }
+
+    class ColdPrep : KitchenSection
+    {
+        public FoodItem PrepDish(int dishId)
+        {
+            return new FoodItem()
+            {
+                DishId = dishId
+            };
+        }
+    }
+
+    class HotPrep : KitchenSection
+    {
+        public FoodItem PrepDish(int dishId)
+        {
+            return new FoodItem()
+            {
+                DishId = dishId
+            };
+        }
+    }
+
+    class Bar : KitchenSection
+    {
+        public FoodItem PrepDish(int dishId)
+        {
+            return new FoodItem()
+            {
+                DishId = dishId
+            };
+        }
+    }
+
+    class Server
+    {
+        private ColdPrep _coldPrep = new ColdPrep();
+        private Bar _bar = new Bar();
+        private HotPrep _hotPrep = new HotPrep();
+
+        public Order PlaceOrder(Patron patron, int coldId, int hotId, int drinkId)
+        {
+            Console.WriteLine($"{patron.Name} place order for cold app #{coldId}, hot entree #{hotId} adn drinks # {drinkId}.");
+
+            var order = new Order
+            {
+                Appetizer = _coldPrep.PrepDish(coldId),
+                Entree = _hotPrep.PrepDish(hotId),
+                Drink = _bar.PrepDish(drinkId)
+            };
+
+            return order;
+        }
+    }
+}
